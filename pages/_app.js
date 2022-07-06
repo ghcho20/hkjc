c8df026a98738d74783f3ee12eab7eb5c5cd0544
sendnerd <gunho.cho@sendbird.com>
2022-07-07 05:39:26 +0900

    prep SendbirdProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,21 @@
 import '../styles/globals.css'
 
 import Navi from '../components/Navi'
+import dynamic from 'next/dynamic'
+const SendbirdProvider = dynamic(() => import('@sendbird/uikit-react/SendbirdProvider'), {
+  ssr: false
+})
 
 function MyApp({ Component, pageProps }) {
+  const appId = process.env.NEXT_PUBLIC_APP_ID
+
   return (
     <div className='grid w-screen h-screen justify-center content-center'>
       <div className='grid justify-center content-center rounded-3xl w-[420px] h-[780px] bg-stone-700'>
         <div className='rounded-t-md w-[400px] h-[670px] bg-white'>
-          <Component {...pageProps} />
+          <SendbirdProvider appId={appId} userId='call01'>
+            <Component {...pageProps} />
+          </SendbirdProvider>
         </div>
         <div className='rounded-b-md w-[400px] h-[60px] bg-white'>
           <Navi name='home' />
--- pages/account.js
+++ pages/account.js
@@ -1,7 +1,12 @@
+import { sendBirdSelectors, useSendbirdStateContext } from "@sendbird/uikit-react"
+
 export default function Account() {
+    const context = useSendbirdStateContext()
+    const sdk = sendBirdSelectors.getSdk(context)
+    const uid = sdk.getCurrentUserId()
     return (
         <div className='flex w-full h-full justify-center items-center'>
-            Login
+            <div>Login: {uid}</div>
         </div>
     )
 }
