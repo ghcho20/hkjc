6d7064d6c2741169cb032e8fa89fa8dd4c75a7eb
sendnerd <gunho.cho@sendbird.com>
2022-07-10 03:56:00 +0900

    custom window title

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import '../styles/globals.css'
 import '@sendbird/uikit-react/dist/index.css'
 import '../styles/sendbird.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const title = process.env.NEXT_PUBLIC_WINDOW_TITLE
+  console.log('title: ', title)
+  return <div>
+      <Head><title>{title}</title></Head>
+      <Component {...pageProps} />
+    </div>
 }
 
 export default MyApp
