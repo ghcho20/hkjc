6d8a6794954400bf0093d62d76b0fa4fdb58d211
sendnerd <gunho.cho@sendbird.com>
2022-07-09 16:51:33 +0900

    message search

--- components/ChatHeader.js
+++ components/ChatHeader.js
@@ -5,9 +5,12 @@ import Image from "next/image";
 import { AiFillSetting } from 'react-icons/ai'
 import { AiOutlineFileSearch } from "react-icons/ai";
 
-const ChatHeader = ({ channel, onBack, setSettings }) => {
+const ChatHeader = ({ channel,
+                      onBack,
+                      setSettings,
+                      setShowSearch }) => {
     const sbState = useSendbirdStateContext()
-    const { user } = sbState.stores.userStore
+    // const { user } = sbState.stores.userStore
     // console.log('user ', user.userId, user.nickname, user.profileUrl)
 
     return (
@@ -18,8 +21,7 @@ const ChatHeader = ({ channel, onBack, setSettings }) => {
             <span>{channel.name? channel.name : `Group of ${channel.memberCount}`}</span>
             <span className="inline-flex space-x-4">
             <AiOutlineFileSearch size={30} color='blueviolet'
-                onClick={() => {
-                }}
+                onClick={() => setShowSearch(true)}
             />
             <AiFillSetting size={30} color='blueviolet'
                 onClick={() => setSettings(true)}
--- components/group.js
+++ components/group.js
@@ -1,15 +1,19 @@
 import ChannelList from '@sendbird/uikit-react/ChannelList'
 import Channel from '@sendbird/uikit-react/Channel'
-import ChatHeader from "../components/ChatHeader";
-import ChannelPreview from "../components/ChannelPreview";
 import ChannelSettings from '@sendbird/uikit-react/ChannelSettings'
-import LeaveChannel from '@sendbird/uikit-react/ChannelSettings/components/LeaveChannel'
+import MessageSearch from '@sendbird/uikit-react/MessageSearch'
 
 import { useState } from "react";
+
+import ChatHeader from "./ChatHeader";
+import ChannelPreview from "./ChannelPreview";
 
 export default function Group() {
   const [channel, setChannel] = useState(null)
   const [settings, setSettings] = useState(false)
+  const [showSearch, setShowSearch] = useState(false)
+  const [startingPoint, setStartingPoint] = useState(0)
+  const [highlightedMessage, setHighlightedMessage] = useState(0)
 
   const onChannelSelect = (_channel) => {
     setChannel(_channel);
@@ -21,20 +25,25 @@ export default function Group() {
 
   return (
     <div className="App w-full h-full">
-        {!settings && channel && (
+        {!settings && !showSearch && channel && (
           <Channel
             channelUrl={channel.url}
             useReaction={true}
+            replyType='QUOTE_REPLY'
+            useMessageGrouping={true}
+            highlightedMessage={highlightedMessage}
+            startingPoint={startingPoint}
             renderChannelHeader={() => (
               <ChatHeader
                 channel={channel}
                 onBack={onBack}
                 setSettings={setSettings}
+                setShowSearch={setShowSearch}
               />
             )}
           />
         )}
-        {!settings && !channel && (
+        {!settings && !showSearch && !channel && (
           <ChannelList
             allowProfileEdit={true}
             renderChannelPreview={({channel}) => {
@@ -57,6 +66,17 @@ export default function Group() {
           <ChannelSettings
             channelUrl={channel.url}
             onCloseClick={() => setSettings(false)}
+          />
+        )}
+        {showSearch && (
+          <MessageSearch
+            channelUrl={channel.url}
+            onCloseClick={() => setShowSearch(false)}
+            onResultClick={(msg) => {
+              setStartingPoint(msg.createdAt)
+              setHighlightedMessage(msg.messageId)
+              setShowSearch(false)
+            }}
           />
         )}
     </div>
